81c7ae45f22d82ed6ad0ac26abaa120c33404483
ikuchmin <ikuchmin@gmail.com>
2023-09-11 12:25:08 +0400

    Use DelegatingWebMvcConfiguration to support multiple WebMvcConfigurer

--- src/main/java/com/haulmont/exceptionhandlingdemo/common/ExtendedWebMvcConfigurationSupport.java
+++ src/main/java/com/haulmont/exceptionhandlingdemo/common/ExtendedWebMvcConfigurationSupport.java
@@ -2,14 +2,11 @@ package com.haulmont.exceptionhandlingdemo.common;
 
 import com.haulmont.exceptionhandlingdemo.common.resolver.AdvancedExceptionHandlerExceptionResolver;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.HandlerExceptionResolver;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.DelegatingWebMvcConfiguration;
 import org.springframework.web.servlet.mvc.method.annotation.ExceptionHandlerExceptionResolver;
 
-import java.util.List;
-
 // @Configuration
-public class ExtendedWebMvcConfigurationSupport extends WebMvcConfigurationSupport {
+public class ExtendedWebMvcConfigurationSupport extends DelegatingWebMvcConfiguration {
 
     @Override
     protected ExceptionHandlerExceptionResolver createExceptionHandlerExceptionResolver() {
